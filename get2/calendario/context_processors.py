0aa1dc7e74be7a5380e2d6b2bd37abb88751cab5
luk156 <matteo@luccalug.it>
2013-07-17 14:13:26 +0200

    titolo e footer

--- get2/calendario/context_processors.py
+++ get2/calendario/context_processors.py
@@ -1,4 +1,8 @@
+import get2.calendario.settings_calendario as settings_calendario
 
 def calendari(request):
     from get2.calendario.models import Calendario
     return {'calendari': Calendario.objects.all()}
+
+def titolo(request):
+	return {'titolo': settings_calendario.TITOLO }
