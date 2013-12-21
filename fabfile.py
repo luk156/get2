4e3d5633527b6c68a9b350ded86afc29f2b039d4
Matteo Lucchesi <matteo@luccalug.it>
2013-12-21 20:39:19 +0100

    aggiornato settings

--- fabfile.py
+++ fabfile.py
@@ -17,16 +17,29 @@ def deploy(name):
 	else:
 		run("mkdir ~/%s" % name)
 		with cd("~/%s" % name):
-			run("git clone https://github.com/luk156/get2.git")
-			with cd("~/%s/get2" % name):
-				configure(name)
+			fabtools.require.git.working_copy("https://github.com/luk156/get2.git")
+			password = create_database(name)
+			with cd("get2"):
+				configure(name,password)
 			fabtools.require.service.restarted('apache2')
 
-def configure(name):
-	print name
+def configure(name, password):
+	run ("cp get2/settings.py.sample get2/settings.py")
+	sed("get2/settings.py", "_database_", name)
+	sed("get2/settings.py", "_user_", name)
+	sed("get2/settings.py", "_titolo_", name)
+	sed("get2/settings.py", "_password_", password)
 
 def create_database(name):
 	with settings(mysql_user='root', mysql_password='Franchini03'):
 		password = prompt('Inserisci la password del nuovo database: ')
 		fabtools.require.mysql.user(name, password)
 		fabtools.require.mysql.database(name, owner=name)
+		return password
+
+def update(name):
+	with cd("~/%s/" % name):
+		fabtools.require.git.working_copy("https://github.com/luk156/get2.git")
+		with fabtools.python.virtualenv('../stable-env'):
+			run("python get2/manage.py collectstatic")
+	fabtools.require.service.restarted('apache2')
