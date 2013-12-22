be373bc40712bc7d3d9516e8428bb229351872f8
Matteo Lucchesi <matteo@luccalug.it>
2013-12-22 18:18:26 +0100

    continuato lavoro su deploy automatico

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,8 @@ import fabtools
 env.shell = "/bin/bash -c -l"
 env.hosts = ['django@luccalug.it',]
 
+virtualenv_dir = "/home/django/stable-env/"
+
 def runserver():
 	with prefix("source ~/.bashrc"):
 		with prefix("workon venv-get"):
@@ -21,10 +23,15 @@ def deploy(name):
 			password = create_database(name)
 			with cd("get2"):
 				configure(name,password)
-			with fabtools.python.virtualenv('../stable-env'):
-				run("python get2/manage.py collectstatic")
-				run("python get2/manage.py syncdb")
-				run("python get2/manage.py migrate")
+				with fabtools.python.virtualenv(virtualenv_dir):
+					run("python get2/manage.py syncdb")
+					run("python get2/manage.py migrate")
+					run("python get2/manage.py collectstatic")
+				run ("cp get2/get2.wsgi.sample get2/get2.wsgi")
+				sed("get2/get2.wsgi", "_envdir_", virtualenv_dir)
+				sed("get2/virtualhost.sample", "_name_", name)
+				sudo ("cp get2/virtualhost.sample /etc/apache2/sites-available/%s" % name)
+				require.apache.enable_site(name)
 		fabtools.require.service.restarted('apache2')
 
 
@@ -45,6 +52,7 @@ def create_database(name):
 def update(name):
 	with cd("~/%s/" % name):
 		fabtools.require.git.working_copy("https://github.com/luk156/get2.git")
-		with fabtools.python.virtualenv('../stable-env'):
+		with fabtools.python.virtualenv(virtualenv_dir):
+			run("python get2/manage.py migrate")
 			run("python get2/manage.py collectstatic")
 	fabtools.require.service.restarted('apache2')
