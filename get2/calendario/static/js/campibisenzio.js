7a23802bfc3f24082418e4896bf3df3a1902ad96
luk156 <matteo@luccalug.it>
2014-04-04 21:23:00 +0200

    rimosse capacita calendario e personalizzazioni campi

--- get2/calendario/static/js/campibisenzio.js
+++ get2/calendario/static/js/campibisenzio.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     var div_persone = $('#persone');
     $('.box-header.persone > .btn-group', div_persone).html('<a href="" class="btn"><i class="icon-refresh"> </i> Sincronizza</a>"')
-    $('td[data-title=Operazioni]', div_persone).html('')
+    $('td[data-title=Operazioni]', div_persone).html('');
+    var th=0;
+    var h=0;
+    if ($(window).width()>756){
+     for (var i=1;i<9;i++)
+        {
+            var h_max=0;
+            $(".turno."+i).each(function(){
+                th = $(this).height();
+                h=$(this).offset().top + th + 8;
+                if (h>h_max){h_max=h;}
+            });
+            $(".turno."+(i+1).toString()).each(function(){ $(this).offset({top:h_max}) });
+        }
+    };
+    $('.row-fluid.calendario').height(h-th-8);
 });
