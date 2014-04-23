aad9775bfccc06850145e80b6aa340c1550bc61e
luk156 <matteo@luccalug.it>
2014-04-23 23:26:15 +0200

    inclusa personalizzazione cv nel codice

--- get2/calendario/static/js/croce_verde.js
+++ get2/calendario/static/js/croce_verde.js
@@ -1,16 +1,3 @@
-$(document).ready(function() {
-    $('.persona-turno.hide').each(function() {
-        if ($(this).parent().parent().hasClass('persone-requisito-6')) {
-            $(this).removeClass('hide');
-        }
-    });
-    window.setTimeout(function() {
-        $('.persona-mansione-turno:not(.hide)').each(function() {
-            if ($(this).attr('data-content').search('icon-ambulance') != -1) {
-                this.insertAdjacentHTML('beforeend', '<i class="icon-ambulance"></i>');
-            };
-        });
-    }, 200);
-});
+
 
 
