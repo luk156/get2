eac989ddeaa032e28344f52dedc17f6d44398293
luk156 <matteo@luccalug.it>
2014-08-19 21:09:50 +0200

    fix

--- persone/models.py
+++ persone/models.py
@@ -19,93 +19,93 @@ class GetModelManager(models.Manager):
 # Create your models here.
 
 class MultiSelectFormField(forms.MultipleChoiceField):
-    widget = forms.CheckboxSelectMultiple
+	widget = forms.CheckboxSelectMultiple
  
-    def __init__(self, *args, **kwargs):
-        self.max_choices = kwargs.pop('max_choices', 0)
-        super(MultiSelectFormField, self).__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		self.max_choices = kwargs.pop('max_choices', 0)
+		super(MultiSelectFormField, self).__init__(*args, **kwargs)
  
-    def clean(self, value):
-        if not value and self.required:
-            raise forms.ValidationError(self.error_messages['required'])
-        # if value and self.max_choices and len(value) > self.max_choices:
-        #     raise forms.ValidationError('You must select a maximum of %s choice%s.'
-        #             % (apnumber(self.max_choices), pluralize(self.max_choices)))
-        return value
+	def clean(self, value):
+		if not value and self.required:
+			raise forms.ValidationError(self.error_messages['required'])
+		# if value and self.max_choices and len(value) > self.max_choices:
+		#     raise forms.ValidationError('You must select a maximum of %s choice%s.'
+		#             % (apnumber(self.max_choices), pluralize(self.max_choices)))
+		return value
 
  
 class MultiSelectField(models.Field):
-    __metaclass__ = models.SubfieldBase
+	__metaclass__ = models.SubfieldBase
  
-    def get_internal_type(self):
-        return "CharField"
+	def get_internal_type(self):
+		return "CharField"
  
-    def get_choices_default(self):
-        return self.get_choices(include_blank=False)
+	def get_choices_default(self):
+		return self.get_choices(include_blank=False)
  
-    def _get_FIELD_display(self, field):
-        value = getattr(self, field.attname)
-        choicedict = dict(field.choices)
+	def _get_FIELD_display(self, field):
+		value = getattr(self, field.attname)
+		choicedict = dict(field.choices)
  
-    def formfield(self, **kwargs):
-        # don't call super, as that overrides default widget if it has choices
-        defaults = {'required': not self.blank, 'label': capfirst(self.verbose_name),
-                    'help_text': self.help_text, 'choices': self.choices}
-        if self.has_default():
-            defaults['initial'] = self.get_default()
-        defaults.update(kwargs)
-        return MultiSelectFormField(**defaults)
+	def formfield(self, **kwargs):
+		# don't call super, as that overrides default widget if it has choices
+		defaults = {'required': not self.blank, 'label': capfirst(self.verbose_name),
+					'help_text': self.help_text, 'choices': self.choices}
+		if self.has_default():
+			defaults['initial'] = self.get_default()
+		defaults.update(kwargs)
+		return MultiSelectFormField(**defaults)
 
-    def get_prep_value(self, value):
-        return value
+	def get_prep_value(self, value):
+		return value
 
-    def get_db_prep_value(self, value, connection=None, prepared=False):
-        if isinstance(value, basestring):
-            return value
-        elif isinstance(value, list):
-            return ",".join(value)
+	def get_db_prep_value(self, value, connection=None, prepared=False):
+		if isinstance(value, basestring):
+			return value
+		elif isinstance(value, list):
+			return ",".join(value)
  
-    def to_python(self, value):
-        if value is not None:
-            return value if isinstance(value, list) else value.split(',')
-        return ''
+	def to_python(self, value):
+		if value is not None:
+			return value if isinstance(value, list) else value.split(',')
+		return ''
 
-    def contribute_to_class(self, cls, name):
-        super(MultiSelectField, self).contribute_to_class(cls, name)
-        if self.choices:
-            func = lambda self, fieldname = name, choicedict = dict(self.choices): ",".join([choicedict.get(value, value) for value in getattr(self, fieldname)])
-            setattr(cls, 'get_%s_display' % self.name, func)
+	def contribute_to_class(self, cls, name):
+		super(MultiSelectField, self).contribute_to_class(cls, name)
+		if self.choices:
+			func = lambda self, fieldname = name, choicedict = dict(self.choices): ",".join([choicedict.get(value, value) for value in getattr(self, fieldname)])
+			setattr(cls, 'get_%s_display' % self.name, func)
  
-    def validate(self, value, model_instance):
-        arr_choices = self.get_choices_selected(self.get_choices_default())
-        for opt_select in value:
-            if (int(opt_select) not in arr_choices):  # the int() here is for comparing with integer choices
-                raise exceptions.ValidationError(self.error_messages['invalid_choice'] % value)  
-        return
+	def validate(self, value, model_instance):
+		arr_choices = self.get_choices_selected(self.get_choices_default())
+		for opt_select in value:
+			if (int(opt_select) not in arr_choices):  # the int() here is for comparing with integer choices
+				raise exceptions.ValidationError(self.error_messages['invalid_choice'] % value)  
+		return
  
-    def get_choices_selected(self, arr_choices=''):
-        if not arr_choices:
-            return False
-        list = []
-        for choice_selected in arr_choices:
-            list.append(choice_selected[0])
-        return list
+	def get_choices_selected(self, arr_choices=''):
+		if not arr_choices:
+			return False
+		list = []
+		for choice_selected in arr_choices:
+			list.append(choice_selected[0])
+		return list
  
-    def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
-        return self.get_db_prep_value(value)
+	def value_to_string(self, obj):
+		value = self._get_val_from_obj(obj)
+		return self.get_db_prep_value(value)
 
 class SelfForeignKey(models.ForeignKey):
-    def pre_save(self, instance, add):
-        manager = instance.__class__.objects
-        ancestor_id = getattr(instance, self.attname)
-        while ancestor_id is not None:
-            if ancestor_id == instance.id:
-                return None
-            ancestor = manager.get(id=ancestor_id)
-            ancestor_id = getattr(ancestor, self.attname)
-        return getattr(instance, self.attname)
-        
+	def pre_save(self, instance, add):
+		manager = instance.__class__.objects
+		ancestor_id = getattr(instance, self.attname)
+		while ancestor_id is not None:
+			if ancestor_id == instance.id:
+				return None
+			ancestor = manager.get(id=ancestor_id)
+			ancestor_id = getattr(ancestor, self.attname)
+		return getattr(instance, self.attname)
+		
 from south.modelsinspector import add_introspection_rules  
 add_introspection_rules([], ["^persone\.models\.MultiSelectField"]) 
 add_introspection_rules([], ["^persone\.models\.SelfForeignKey"]) 
@@ -115,17 +115,17 @@ STATI=(('disponibile','Disponibile'),('ferie','In ferie'),('malattia','In malatt
 GIORNI=((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'))
 
 ICONE = (
-    ('icon-user','icon-user'),
-    ('icon-ambulance','icon-ambulance'),
-    ('icon-truck','icon-truck'),
-    ('icon-user-md','icon-user-md'),
-    ('icon-phone','icon-phone'),
-    ('icon-stethoscope','icon-stethoscope'),
-    ('icon-eye-open','icon-eye-open'),
-    ('icon-bolt','icon-bolt'),
-    ('icon-male','icon-male'),
-    ('icon-female','icon-female'),
-    )
+	('icon-user','icon-user'),
+	('icon-ambulance','icon-ambulance'),
+	('icon-truck','icon-truck'),
+	('icon-user-md','icon-user-md'),
+	('icon-phone','icon-phone'),
+	('icon-stethoscope','icon-stethoscope'),
+	('icon-eye-open','icon-eye-open'),
+	('icon-bolt','icon-bolt'),
+	('icon-male','icon-male'),
+	('icon-female','icon-female'),
+	)
 
 class Mansione(models.Model):
 	nome =models.CharField('Nome',max_length=30)
@@ -146,48 +146,48 @@ class Mansione(models.Model):
 		return True
 	def figli(self):
 		return Mansione.objectsGet.filter(padre=self)
-    def save(self, *args, **kwargs):
-        for p in Persona.objects.all():
-            cache.delete('capacita_'+str(p.id))
-        super(Mansione, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		for p in Persona.objects.all():
+			cache.delete('capacita_'+str(p.id))
+		super(Mansione, self).save(*args, **kwargs)
 
 def figli(mansione_id):
-    mansioni = Mansione.objectsGet.all().values_list('id','padre')
-    f = [v for i, v in enumerate(mansioni) if v[1] == mansione_id]
-    figli_list = f
-    while f:
-        for k in f:
-            f=[v for i, v in enumerate(mansioni) if v[1] == k[0]]
-            figli_list+=f
-    return Mansione.objects.filter( id__in=[x for (x,y) in figli_list] )
+	mansioni = Mansione.objectsGet.all().values_list('id','padre')
+	f = [v for i, v in enumerate(mansioni) if v[1] == mansione_id]
+	figli_list = f
+	while f:
+		for k in f:
+			f=[v for i, v in enumerate(mansioni) if v[1] == k[0]]
+			figli_list+=f
+	return Mansione.objects.filter( id__in=[x for (x,y) in figli_list] )
 
 
 
 class MansioneForm(forms.ModelForm):
-    class Meta:
-        model = Mansione
-    def __init__(self, *args, **kwargs):
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Field('nome'),
-            Field('descrizione'),
-            Field('padre'),
-            Field(
-                'colore',
-                template = 'form_templates/color.html'
-            ),
-            Field(
-                'icona',
-                template = 'form_templates/radioselect_inline.html',
-            ),
-            Field('escludi_stat'),
-            Field('ereditabile'),
-            FormActions(
-                Submit('save', 'Invia', css_class="btn-primary")
-            )
-        )
-        super(MansioneForm, self).__init__(*args, **kwargs)
-        self.fields['padre'].queryset = Mansione.objectsGet.exclude(id__exact=self.instance.id)
+	class Meta:
+		model = Mansione
+	def __init__(self, *args, **kwargs):
+		self.helper = FormHelper()
+		self.helper.layout = Layout(
+			Field('nome'),
+			Field('descrizione'),
+			Field('padre'),
+			Field(
+				'colore',
+				template = 'form_templates/color.html'
+			),
+			Field(
+				'icona',
+				template = 'form_templates/radioselect_inline.html',
+			),
+			Field('escludi_stat'),
+			Field('ereditabile'),
+			FormActions(
+				Submit('save', 'Invia', css_class="btn-primary")
+			)
+		)
+		super(MansioneForm, self).__init__(*args, **kwargs)
+		self.fields['padre'].queryset = Mansione.objectsGet.exclude(id__exact=self.instance.id)
 
 
 class Persona(models.Model):
@@ -223,22 +223,22 @@ class Persona(models.Model):
 			return False
 	@cached_property
 	def capacita(self):
-        if cache.get('capacita_'+str(self.id)) != None:
-            return cache.get('capacita_'+str(self.id))
-        else:
-    		c = set()
-    		for m in self.competenze.filter(cancellata=False):
-    			c.add(m)
-    			for f in figli(m.id):
-    				if (f.ereditabile):
-    					c.add(f)
-            cache.set('capacita_'+str(self.id), c)
+		if cache.get('capacita_'+str(self.id)) != None:
+			return cache.get('capacita_'+str(self.id))
+		else:
+			c = set()
+			for m in self.competenze.filter(cancellata=False):
+				c.add(m)
+				for f in figli(m.id):
+					if (f.ereditabile):
+						c.add(f)
+			cache.set('capacita_'+str(self.id), c)
 		return c
 	def __unicode__(self):
 		return '%s %s' % (self.cognome,self.nome)
-    def save(self, *args, **kwargs):
-        cache.delete('capacita_'+str(self.id))
-        super(Persona, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		cache.delete('capacita_'+str(self.id))
+		super(Persona, self).save(*args, **kwargs)
 
 class PersonaForm(forms.ModelForm):
 	class Meta:
@@ -289,73 +289,73 @@ class PersonaForm(forms.ModelForm):
 		self.fields['competenze'].help_text = ''
 
 class PersonaFormLite(forms.ModelForm):
-    class Meta:
-        model = Persona
-        exclude = ('competenze', 'user')
-    def __init__(self, *args, **kwargs):
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            HTML('<div class="row">'),
-            Div(
-                Fieldset(
-                    'Informazioni Anagrafiche',
-                    'nome',
-                    'cognome',
-                    'indirizzo',
-                    ),
-                AppendedText('nascita', '<i class="icon-calendar"></i>'),
-                AppendedText('tel1', '<i class="icon-phone"></i>'),
-                AppendedText('tel2', '<i class="icon-phone"></i>'),
-                AppendedText('tel3', '<i class="icon-phone"></i>'),
-                css_class="span3",
-                ),
-            Div(
-                Fieldset(
-                    'Altre informazioni',
-                    'stato',
-                    ),
-                css_class="span3"
-            ),
-            Fieldset(
-                'Notifiche via E-mail',
-                'notificaMail',
-                AppendedText('giorniNotificaMail', '<i class="icon-envelope"></i>'),
-                ),
-            HTML('</div>'),
-            FormActions(
-                Submit('save', 'Invia', css_class="btn-primary"),
-                )
-        )
-        super(PersonaFormLite, self).__init__(*args, **kwargs)
+	class Meta:
+		model = Persona
+		exclude = ('competenze', 'user')
+	def __init__(self, *args, **kwargs):
+		self.helper = FormHelper()
+		self.helper.layout = Layout(
+			HTML('<div class="row">'),
+			Div(
+				Fieldset(
+					'Informazioni Anagrafiche',
+					'nome',
+					'cognome',
+					'indirizzo',
+					),
+				AppendedText('nascita', '<i class="icon-calendar"></i>'),
+				AppendedText('tel1', '<i class="icon-phone"></i>'),
+				AppendedText('tel2', '<i class="icon-phone"></i>'),
+				AppendedText('tel3', '<i class="icon-phone"></i>'),
+				css_class="span3",
+				),
+			Div(
+				Fieldset(
+					'Altre informazioni',
+					'stato',
+					),
+				css_class="span3"
+			),
+			Fieldset(
+				'Notifiche via E-mail',
+				'notificaMail',
+				AppendedText('giorniNotificaMail', '<i class="icon-envelope"></i>'),
+				),
+			HTML('</div>'),
+			FormActions(
+				Submit('save', 'Invia', css_class="btn-primary"),
+				)
+		)
+		super(PersonaFormLite, self).__init__(*args, **kwargs)
 		self.fields['competenze'].help_text = ''
 
 class Gruppo(models.Model):
-    nome = models.CharField('Nome',max_length=30)
-    componenti = models.ManyToManyField(Persona, blank=True, null=True, related_name='componenti_gruppo')
-    note = models.TextField( blank=True, null=True, )
-    escludi_stat = models.BooleanField('Escludi dalle statistiche', default=False,  help_text="le persone appartenenti a questo gruppo saranno escluse dalle statistiche")
-    def numero_componenti(self):
-        n=0
-        for c in self.componenti.all():
-            n+=1
-        return n
-    def __unicode__(self):
-        return '%s' % (self.nome)
+	nome = models.CharField('Nome',max_length=30)
+	componenti = models.ManyToManyField(Persona, blank=True, null=True, related_name='componenti_gruppo')
+	note = models.TextField( blank=True, null=True, )
+	escludi_stat = models.BooleanField('Escludi dalle statistiche', default=False,  help_text="le persone appartenenti a questo gruppo saranno escluse dalle statistiche")
+	def numero_componenti(self):
+		n=0
+		for c in self.componenti.all():
+			n+=1
+		return n
+	def __unicode__(self):
+		return '%s' % (self.nome)
 
 class GruppoForm(forms.ModelForm):
-    #nascita = forms.DateField(label='Data di nascita', required=False, widget=widgets.AdminDateWidget)
-    class Meta:
-        model = Gruppo
-        exclude = ('componenti',)
-    def __init__(self, *args, **kwargs):
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Field('nome'),
-            Field('note'),
-            Field('escludi_stat'),
-            FormActions(
-                Submit('save', 'Invia', css_class="btn-primary")
-            )
-        )
-        super(GruppoForm, self).__init__(*args, **kwargs)
+	#nascita = forms.DateField(label='Data di nascita', required=False, widget=widgets.AdminDateWidget)
+	class Meta:
+		model = Gruppo
+		exclude = ('componenti',)
+	def __init__(self, *args, **kwargs):
+		self.helper = FormHelper()
+		self.helper.layout = Layout(
+			Field('nome'),
+			Field('note'),
+			Field('escludi_stat'),
+			FormActions(
+				Submit('save', 'Invia', css_class="btn-primary")
+			)
+		)
+		super(GruppoForm, self).__init__(*args, **kwargs)
 
