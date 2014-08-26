e82514ec43c6dfd63676167fbc5fd42f7cc5dce8
luk156 <matteo@luccalug.it>
2014-08-26 21:00:27 +0200

    fix

--- get2/calendario/static/js/humanitasfinord.js
+++ get2/calendario/static/js/humanitasfinord.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $('.turno').each(function(){
 		var not_verificato = $('.titolo-turno-False',this).length > 0;
-		if (not_verificato && $('.h6-mansione-10 span.badge-important',this).length==1 && $('.span.badge-important',this).length==1)
+		if (not_verificato && $('.h6-mansione-10 span.badge-important',this).length==1 && $('span.badge-important',this).length==1)
 			$('.titolo-turno',this).css('border','1px solid rgb(252, 101, 0)').css('background','none repeat scroll 0% 0% #F89E3D');
 	});
 
