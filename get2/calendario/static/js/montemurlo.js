b9e1f7af0fce9ddeff6775fd525b4c0b7eacc2f0
luk156 <matteo@luccalug.it>
2014-08-29 22:34:09 +0200

    fix

--- get2/calendario/static/js/montemurlo.js
+++ get2/calendario/static/js/montemurlo.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
 	if(window.staff && !window.superuser ){
 		$('.persone, .utenti, .impostazioni','#menu1').remove();
 		$('.cal-action').remove();
+		$('.edit-turno').remove();
 	}
 });
