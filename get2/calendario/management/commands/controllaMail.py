8ffc42e60f4eadbd726439a71d4057d36657a79b
luk156 <matteo@luccalug.it>
2014-08-29 22:39:38 +0200

    indentaz

--- get2/calendario/management/commands/controllaMail.py
+++ get2/calendario/management/commands/controllaMail.py
@@ -7,83 +7,83 @@ import urllib
 import urllib2
 
 def UrlEncode(recipients):
-		resultString = ''
-		for number in recipients:
-				resultString = resultString + 'recipients[]=' + urllib.quote_plus(number) + '&'
-		return resultString[:-1]
+	resultString = ''
+	for number in recipients:
+			resultString = resultString + 'recipients[]=' + urllib.quote_plus(number) + '&'
+	return resultString[:-1]
 
 def skebbyGatewaySendSMS(username,password,recipients,text,sms_type='basic',sender_number='',sender_string='',charset='ISO-8859-1',options={ 'User-Agent' : 'Generic Client' }):
-		url = 'http://gateway.skebby.it/api/send/smseasy/advanced/http.php'
+	url = 'http://gateway.skebby.it/api/send/smseasy/advanced/http.php'
 
-		method = 'send_sms_basic'
+	method = 'send_sms_basic'
 
-		if sms_type=='classic' : method = 'send_sms_classic'
-		if sms_type=='report' : method = 'send_sms_classic_report'
+	if sms_type=='classic' : method = 'send_sms_classic'
+	if sms_type=='report' : method = 'send_sms_classic_report'
 
-		parameters = {
-				'method' : method,
-				'username' : username,
-				'password' : password,
-				'text' : text
-		}
+	parameters = {
+			'method' : method,
+			'username' : username,
+			'password' : password,
+			'text' : text
+	}
 
-		if sender_number != '' and sender_string != '' :
-				result = {}
-				result['status'] = 'failed'
-				result['message'] = SENDER_ERROR
-				return result
+	if sender_number != '' and sender_string != '' :
+			result = {}
+			result['status'] = 'failed'
+			result['message'] = SENDER_ERROR
+			return result
 
-		if sender_number != '' : parameters['sender_number'] = sender_number
-		if sender_string != '' : parameters['sender_string'] = sender_string
+	if sender_number != '' : parameters['sender_number'] = sender_number
+	if sender_string != '' : parameters['sender_string'] = sender_string
 
-		if charset != 'ISO-8859-1' : parameters['charset'] = 'UTF-8'
+	if charset != 'ISO-8859-1' : parameters['charset'] = 'UTF-8'
 
-		headers = options
-		data = urllib.urlencode(parameters) + '&' + UrlEncode(recipients)
+	headers = options
+	data = urllib.urlencode(parameters) + '&' + UrlEncode(recipients)
 
-		req = urllib2.Request(url, data, headers)
-		try:
-				response = urllib2.urlopen(req)
-		except urllib2.HTTPError as e:
-				result = {}
-				result['status'] = 'failed'
-				result['code'] = e.code
-				result['message'] = NET_ERROR
-				return result
-		except urllib2.URLError as e:
-				result = {}
-				result['status'] = 'failed'
-				result['message'] = e.reason
-				return result
+	req = urllib2.Request(url, data, headers)
+	try:
+			response = urllib2.urlopen(req)
+	except urllib2.HTTPError as e:
+			result = {}
+			result['status'] = 'failed'
+			result['code'] = e.code
+			result['message'] = NET_ERROR
+			return result
+	except urllib2.URLError as e:
+			result = {}
+			result['status'] = 'failed'
+			result['message'] = e.reason
+			return result
 
-		resultString = response.read()
+	resultString = response.read()
 
-		results = resultString.split('&')
-		result = {}
-		for r in results:
-				temp = r.split('=')
-				result[temp[0]] = temp[1]
+	results = resultString.split('&')
+	result = {}
+	for r in results:
+			temp = r.split('=')
+			result[temp[0]] = temp[1]
 
-		return result
+	return result
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-	dataInizio = datetime.date.today()
-	dataInizio = dataInizio + datetime.timedelta(days=1)
-	dataFine = dataInizio + datetime.timedelta(days=7)
-	disponibilita = Disponibilita.objects.filter(turno__inizio__range=(dataInizio, dataFine), tipo="Disponibile")
-	listaMessaggi = ()
-	cell_rule = re.compile('^[3]\d{9}$')
-	for dis in disponibilita:
-		differenzaGiorni = (dis.turno.inizio.date() - datetime.date.today()).days
-		if(dis.persona.user and int(dis.persona.giorniNotificaMail) == differenzaGiorni):
-			messaggio = 'Salve ' + dis.persona.nome + ', ti ricordo che in data ' + dis.turno.inizio.date().strftime('%d/%m/%Y') + ' dalle ore ' + dis.turno.inizio.time().strftime('%H:%M') + ' alle ore ' + dis.turno.fine.time().strftime('%H:%M') + ' dovrai effettuare un turno.'
-			if(dis.persona.user.email != '' and dis.persona.notificaMail):
-				listaMessaggi += (('Get 2.0 - Avviso turno', messaggio, settings.GET_MAIL_NOTIFICA, [dis.persona.user.email]),)
+		dataInizio = datetime.date.today()
+		dataInizio = dataInizio + datetime.timedelta(days=1)
+		dataFine = dataInizio + datetime.timedelta(days=7)
+		disponibilita = Disponibilita.objects.filter(turno__inizio__range=(dataInizio, dataFine), tipo="Disponibile")
+		listaMessaggi = ()
+		cell_rule = re.compile('^[3]\d{9}$')
+		for dis in disponibilita:
+			differenzaGiorni = (dis.turno.inizio.date() - datetime.date.today()).days
+			if(dis.persona.user and int(dis.persona.giorniNotificaMail) == differenzaGiorni):
+				messaggio = 'Salve ' + dis.persona.nome + ', ti ricordo che in data ' + dis.turno.inizio.date().strftime('%d/%m/%Y') + ' dalle ore ' + dis.turno.inizio.time().strftime('%H:%M') + ' alle ore ' + dis.turno.fine.time().strftime('%H:%M') + ' dovrai effettuare un turno.'
+				if(dis.persona.user.email != '' and dis.persona.notificaMail):
+					listaMessaggi += (('Get 2.0 - Avviso turno', messaggio, settings.GET_MAIL_NOTIFICA, [dis.persona.user.email]),)
 
-			if(cell_rule.search(disp.persona.tel1) and dis.persona.notificaSMS):
-				recipients=[]
-				recipients.append('39'+disp.persona.tel1)
-				result = skebbyGatewaySendSMS(settings.GET_SKEBBY_USERNAME,settings.GET_SKEBBY_PASSWORD,recipients,messaggio,'classic','','GeT')
-				print result
-	send_mass_mail(listaMessaggi)
+				if(cell_rule.search(disp.persona.tel1) and dis.persona.notificaSMS):
+					recipients=[]
+					recipients.append('39'+disp.persona.tel1)
+					result = skebbyGatewaySendSMS(settings.GET_SKEBBY_USERNAME,settings.GET_SKEBBY_PASSWORD,recipients,messaggio,'classic','','GeT')
+					print result
+		send_mass_mail(listaMessaggi)
